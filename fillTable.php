0549fdf55d84b9aeef699d28dd28260b3c95f9a3
YohannCharrier <60933713+YohannCharrier@users.noreply.github.com>
2020-06-08 16:43:30 +0200

    up

--- fillTable.php
+++ fillTable.php
@@ -17,13 +17,14 @@ function read($csv){
     fclose($file);
     return $line;
 }
-exec("C:/Users/ychar/AppData/Local/Programs/Python/Python38/python.exe table.py");
+exec("python table.py");
+
 $dsn = 'mysql:host=localhost;port=3306;dbname=projetCIR2';
 $user = 'admin';
 $password = 'password';
 $bdd = connexpdo($dsn,$user,$password);
 
-$fli = 'flights.csv';
+$fli = 'flights2.csv';
 $fli = read($fli);
 for($i=1;$i<count($fli);$i++){
     $query = "INSERT INTO vols VALUES(?,?,?,?,?,?,?,?,?,?,?)";
@@ -31,7 +32,7 @@ for($i=1;$i<count($fli);$i++){
     $insert->execute([$fli[$i][0],$fli[$i][1],$fli[$i][2],$fli[$i][3],$fli[$i][4],$fli[$i][7],$fli[$i][8],$fli[$i][11],$fli[$i][12],$fli[$i][13],$fli[$i][14]]);
 }
 
-$fares = 'fares.csv';
+$fares = 'fares2.csv';
 $fares = read($fares);
 for($i=1;$i<count($fares);$i++){
     $query1 = "INSERT INTO billets VALUES(?,?,?,?,?,?)";
@@ -39,7 +40,7 @@ for($i=1;$i<count($fares);$i++){
     $insert1->execute([$fares[$i][0],$fares[$i][2],$fares[$i][3],$fares[$i][4],$fares[$i][5],$fares[$i][1]]);
 }
 
-$taxes = 'airportsurcharges.csv';
+$taxes = 'airportsurcharges2.csv';
 $taxes = read($taxes);
 for($i=1;$i<count($taxes);$i++){
     $query2 = "INSERT INTO taxes VALUES(?,?,?)";
--- table.py
+++ table.py
@@ -2,7 +2,7 @@
 def readFile(path):
     file = open(path+".csv","r")
     lines = file.readlines()
-    rst = open(path+".csv","w")
+    rst = open(path+"2.csv","w")
     for line in lines:
         li = line.replace(' ','')
         rst.write(li)
