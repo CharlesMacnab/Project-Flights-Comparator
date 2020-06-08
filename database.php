e1e736d4954422054eafbc2358c4126542b297ab
ThomasParragi <66598277+ThomasParragi@users.noreply.github.com>
2020-06-08 17:18:37 +0200

    Create database.php

---
+++ database.php
@@ -0,0 +1,24 @@
+<?php
+  require_once('constants.php');
+
+  //----------------------------------------------------------------------------
+  //--- dbConnect --------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  // Create the connection to the database.
+  // \return False on error and the database otherwise.
+  function dbConnect()
+  {
+    try
+    {
+      $db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_NAME.';charset=utf8',
+        DB_USER, DB_PASSWORD);
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    }
+    catch (PDOException $exception)
+    {
+      error_log('Connection error: '.$exception->getMessage());
+      return false;
+    }
+    return $db;
+  }
+?>
