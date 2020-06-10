ed8eba2ef91608035e8d6553554701a14a7f9080
Mael1133 <66597772+Mael1133@users.noreply.github.com>
2020-06-10 08:57:57 +0200

    Add files via upload

---
+++ js/ajax.js
@@ -0,0 +1,68 @@
+/**
+ * @Author: Thibault Napoléon <Imothep>
+ * @Company: ISEN Yncréa Ouest
+ * @Email: thibault.napoleon@isen-ouest.yncrea.fr
+ * @Created Date: 23-Jan-2018 - 17:00:53
+ * @Last Modified: 08-Dec-2019 - 19:12:57
+ */
+
+'use strict';
+
+//------------------------------------------------------------------------------
+//--- ajaxRequest --------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Perform an Ajax request.
+// \param type The type of the request (GET, DELETE, POST, PUT).
+// \param url The url with the data.
+// \param callback The callback to call where the request is successful.
+// \param data The data associated with the request.
+function ajaxRequest(type, url, callback= null, data = null)
+{
+    let xhr;
+
+    // Create XML HTTP request.
+    xhr = new XMLHttpRequest();
+    if (type == 'GET' && data != null)
+        url += '?' + data;
+    xhr.open(type, url);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    // Add the onload function.
+    xhr.onload = () =>
+    {
+        httpErrors(xhr.status);
+        if (callback!=null)
+            callback(JSON.parse(xhr.responseText));
+    };
+
+    // Send XML HTTP request.
+    xhr.send();
+}
+
+//------------------------------------------------------------------------------
+//--- httpErrors ---------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Display an error message accordingly to an error code.
+// \param errorCode The error code (HTTP status for example).
+function httpErrors(errorCode)
+{
+    let messages = {
+        10: 'Incorrect login, there is already a user with this login',
+        11: 'Incorrect password',
+        12: 'Incorrect login',
+        400: 'Requête incorrecte',
+        401: 'Authentifiez vous',
+        403: 'Accès refusé',
+        404: 'Page non trouvée',
+        500: 'Erreur interne du serveur',
+        503: 'Service indisponible'
+    };
+
+    // Display error.
+    if (errorCode in messages)
+    {
+        $('#errors').html('<i class="fa fa-exclamation-circle"></i> <strong>' +
+            messages[errorCode] + '</strong>');
+        $('#errors').show();
+    }
+}
---
+++ php/script/controlleur.php
@@ -0,0 +1,139 @@
+<?php
+
+session_start();
+
+if($_GET['func'] == 'suiteRecherche'){
+    suiteRecherche($_POST['number']);
+}
+
+function connexion($base, $user, $password){
+
+    try {
+        $dbh = new PDO($base, $user, $password);
+    } catch (PDOException $e) {
+        echo 'Connexion échouée : ' . $e->getMessage();
+    }
+    return $dbh;
+
+}
+
+function suiteRecherche($number){
+
+    $dbh = connexion('pgsql:host=localhost;dbname=etudiants', 'postgres', 'isen2018');
+
+
+    if($number == 1){
+        <form>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputEmail4">Nom</label>
+                    <input type="text" class="form-control" id="inputEmail4">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputPassword4">Prénom</label>
+                    <input type="text" class="form-control" id="inputPassword4">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputEmail4">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputPassword4">Password</label>
+                    <input type="password" class="form-control" id="inputPassword4">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputAddress">Address</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+            </div>
+            <div class="form-group">
+                <label for="inputAddress2">Address 2</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                <label for="inputCity">City</label>
+                <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                <label for="inputState">State</label>
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+                </div>
+                <div class="form-group col-md-2">
+                <label for="inputZip">Zip</label>
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="gridCheck">
+                <label class="form-check-label" for="gridCheck">
+                    Check me out
+                </label>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Sign in</button>
+        </form>
+    }
+
+    if($number == 2){
+        
+    }
+
+    if($number == 3){
+        
+    }
+
+    if($number == 4){
+        
+    }
+
+    if($number == 5){
+        
+    }
+
+    if($number == 6){
+        
+    }
+
+    if($number == 7){
+        
+    }
+
+    if($number == 8){
+        
+    }
+
+    if($number == 9){
+        
+    }
+
+
+}
+
+function infoConfirmation($bdd){
+    
+    $qry = "SELECT nom,prenom,age INTO Passager WHERE idReservation=?";
+    $slct = $bdd->prepare($qry);
+    $slct->execute([$_SESSION["idCommande"]]);
+    
+    $passagers = $slct->fetchAll();
+    
+    $qry2 = "SELECT INTO Fly WHERE ID=?";
+    $slct2 = $bdd->prepare($qry2);
+    $slct2->execute([$passagers[0]["ID_FLY"]]);
+    $vol = $slct2->fetch();
+
+    $data = array($array($_SESSION["nbPass"],$_SESSION["id_Vol"],$_SESSION["villeDep"],$_SESSION["villeArr"],$_SESSION["heureDep"],$vol["dateToDeparture"],$_SESSION["prix"]),$passagers)
+    
+    $json = json_encode($data);
+    echo $json;
+
+}
+
+?>
