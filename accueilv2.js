f870c9cc972ed9fa67ffb4344b7d2c4e87ece898
YohannCharrier <60933713+YohannCharrier@users.noreply.github.com>
2020-06-10 14:30:17 +0200

    up

--- accueilv2.js
+++ accueilv2.js
@@ -1,13 +1,24 @@
+var numberPassenger;
+
 function createFormPassenger(data){
     
-    console.log(data)
-    let numberPassenger = data[0];
+    numberPassenger = data[0];
     var i=1;
-    console.log(numberPassenger);
     for (i; i<=numberPassenger; i++){
         $('#formPassenger').append("<div class=\"row\"><h1>Passager N°" + i + "</h1> </div><div class=\"form-row\"> <div class=\"form-group col-md-6\"> <label for=\"nom"+i+"\">Nom</label> <input type=\"text\" class=\"form-control\" id=\"nom"+i+"\" required> </div><div class=\"form-group col-md-6\"><label for=\"prenom"+i+"\">Prénom</label><input type=\"text\" class=\"form-control\" id=\"prenom"+i+"\" required></div></div><div class=\"form-group\"><label for=\"email"+i+"\">Email</label><input type=\"email\" class=\"form-control\" id=\"email"+i+"\" required></div><div class=\"form-group\"><label for=\"date"+i+"\">Date de Naissance</label><input type=\"date\" id=\"date"+i+"\" name=\"trip-start\" required></div><br>")
     }
     
+}
+
+function setPassager() {
+    let passengers = Array();
+    for(let i=1; i<=numberPassenger;i++){
+        let nom = document.getElementById("nom"+i).value;
+        let prenom = document.getElementById("nom"+i).value;
+        let mail = document.getElementById("nom"+i).value;
+        let date = document.getElementById("nom"+i).value;
+        passenger = []            
+    }
 }
 
 ajaxRequest("GET", "http://localhost/Projet-CIR2/controlleur.php",createFormPassenger,"func=getInfoVol");
--- controlleur.php
+++ controlleur.php
@@ -17,35 +17,41 @@ function connexion($base, $user, $password){
 
 $bdd = connexion('mysql:host=localhost;port=3306;dbname=projetCIR2','admin','password');
 
-function infoVol(){
+function infoVol($bdd){
 
     $_SESSION['depart'] = $_GET['depAir'];
     $_SESSION['arrive'] = $_GET['arrAir'];
     $_SESSION['dateVol'] = $_GET['date'];
     $_SESSION['nbPass'] = $_GET['nbPass'];
 
-    $tab = array($_SESSION['nbPass'],$_SESSION['depart'],$_SESSION['arrive'],$_SESSION['dateVol']);
-    $json = json_encode($tab);
-    echo $json;
-    /*$qry = "SELECT FROM AirportSurchanges WHERE airportCode=?";
+    $qry = "SELECT city FROM AirportSurchanges WHERE airportCode=?";
 
     $slct = $bdd->prepare($qry);
-    $slct->execute([$_SESSION["depAir"]]);
+    $slct->execute([$_GET['depAir']]);
     $rst = $slct->fetch();
-    $_SESSION["cityDep"] = $rst["city"];
+    $_SESSION['cityDep'] = $rst['city'];
 
     $slct2 = $bdd->prepare($qry);
-    $slct2->execute([$_SESSION["arrAir"]]);
+    $slct2->execute([$_SESSION['arrive']]);
     $rst2 = $slct2->fetch();
-    $_SESSION["cityArr"] = $rst2["city"];*/
+    $_SESSION['cityArr'] = $rst2['city'];
+
+    $tab = array($_SESSION['nbPass'],$_SESSION['depart'],$_SESSION['cityDep'],$_SESSION['arrive'],$_SESSION['cityArr'],$_SESSION['dateVol']);
+    $json = json_encode($tab);
+    echo $json;
 }
 
-function infoPassager(){
-
+function infoPassager($bdd){
+    for($i=1;$i<=$_SESSION['nbPass'];$i++){
+        $query = "INSERT INTO customer VALUES(DEFAULT,?,?,?,?)";
+        $insert = $bdd->prepare($query);
+        $insert->execute([$_GET['nom'.$i],$_GET['prenom'.$i],$_GET['date'.$i],$_GET['mail'.$i]]);
+        $_SESSION['passager'.$i] = array($_GET['nom'.$i],$_GET['prenom'.$i],$_GET['date'.$i],$_GET['mail'.$i]);
+    }
 }
 
 function getInfoVol(){
-    $tab = array($_SESSION['nbPass'],$_SESSION['depart'],$_SESSION['arrive'],$_SESSION['dateVol']);
+    $tab = array($_SESSION['nbPass'],$_SESSION['depart'],$_SESSION['cityDep'],$_SESSION['arrive'],$_SESSION['cityArr'],$_SESSION['dateVol']);
     $json = json_encode($tab);
     echo $json;
 }
@@ -75,7 +81,7 @@ if($_GET["func"]=="infoConfirmation"){
     //infoConfirmation();
 }
 if($_GET["func"]=="infoVol"){
-    infoVol();
+    infoVol($bdd);
 }
 if($_GET["func"]=="getInfoVol"){
     getInfoVol();
