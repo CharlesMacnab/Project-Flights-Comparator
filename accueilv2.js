e3fae1b830517300338469691b018d98710c4967
Charles-Clément Gross <60933720+CharlesMacnab@users.noreply.github.com>
2020-06-10 11:03:51 +0200

    Add files via upload

---
+++ accueilv2.js
@@ -0,0 +1,13 @@
+function createFormPassenger(data){
+    
+    console.log(data)
+    let numberPassenger = data;
+    var i=1;
+    console.log(numberPassenger);
+    for (i; i<=numberPassenger; i++){
+        $('#formPassenger').append("<div class=\"row\"><h1>Passager N°" + i + "</h1> </div><div class=\"form-row\"> <div class=\"form-group col-md-6\"> <label for=\"inputText\">Nom</label> <input type=\"text\" class=\"form-control\" id=\"inputText\" required> </div><div class=\"form-group col-md-6\"><label for=\"inputText\">Prénom</label><input type=\"text\" class=\"form-control\" id=\"inputText\" required></div></div><div class=\"form-group\"><label for=\"inputEmail4\">Email</label><input type=\"email\" class=\"form-control\" id=\"inputEmail4\" required></div><div class=\"form-group\"><label for=\"start\">Date de Naissance</label><input type=\"date\" id=\"start\" name=\"trip-start\" required></div><br>")
+    }
+    
+}
+
+ajaxRequest("GET", "http://localhost/Projet/controlleur.php",createFormPassenger,"func=getPass");
--- controlleur.php
+++ controlleur.php
@@ -17,111 +17,18 @@ function connexion($base, $user, $password){
 
 }
 
-function suiteRecherche($number){
-
-    $dbh = connexion('pgsql:host=localhost;dbname=etudiants', 'postgres', 'isen2018');
-
-
-    if($number == 1){
-        <form>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="inputEmail4">Nom</label>
-                    <input type="text" class="form-control" id="inputEmail4">
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="inputPassword4">Prénom</label>
-                    <input type="text" class="form-control" id="inputPassword4">
-                </div>
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="inputEmail4">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4">
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="inputPassword4">Password</label>
-                    <input type="password" class="form-control" id="inputPassword4">
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="inputAddress">Address</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-            </div>
-            <div class="form-group">
-                <label for="inputAddress2">Address 2</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                <label for="inputCity">City</label>
-                <input type="text" class="form-control" id="inputCity">
-                </div>
-                <div class="form-group col-md-4">
-                <label for="inputState">State</label>
-                <select id="inputState" class="form-control">
-                    <option selected>Choose...</option>
-                    <option>...</option>
-                </select>
-                </div>
-                <div class="form-group col-md-2">
-                <label for="inputZip">Zip</label>
-                <input type="text" class="form-control" id="inputZip">
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck">
-                <label class="form-check-label" for="gridCheck">
-                    Check me out
-                </label>
-                </div>
-            </div>
-            <button type="submit" class="btn btn-primary">Sign in</button>
-        </form>
-    }
-
-    if($number == 2){
-        
-    }
-
-    if($number == 3){
-        
-    }
-
-    if($number == 4){
-        
-    }
-
-    if($number == 5){
-        
-    }
-
-    if($number == 6){
-        
-    }
-
-    if($number == 7){
-        
-    }
-
-    if($number == 8){
-        
-    }
-
-    if($number == 9){
-        
-    }
-
-
+function infoVol($passagers){
+    //if(isset($_POST["depart"],$_POST["arrive"],$_POST["date"],$_POST["passager"])){
+        //$_SESSION["airportD"] = $_POST["depart"];
+        //$_SESSION["airportA"] = $_POST["arrive"];
+        //$_SESSION["dateVol"] = $_POST["date"];
+        $_SESSION["nbPass"] = $_GET["nbPass"];
+    
 }
-function infoVol(){
-    if(isset($_POST["depart"],$_POST["arrive"],$_POST["date"],$_POST["passager"])){
-        $_SESSION["airportD"] = $_POST["depart"];
-        $_SESSION["airportA"] = $_POST["arrive"];
-        $_SESSION["dateVol"] = $_POST["date"];
-        $_SESSION["nbPass"] = $_POST["passager"];
-    }
+
+function getPass(){
+    $json = json_encode($_SESSION["nbPass"]);
+    echo $json;
 }
 
 function infoConfirmation($bdd){
@@ -138,7 +45,7 @@ function infoConfirmation($bdd){
     $vol = $slct2->fetch();
 
 
-    $data = array($array($_SESSION["nbPass"],$_SESSION["id_Vol"],$_SESSION["villeDep"],$_SESSION["villeArr"],$_SESSION["heureDep"],$vol["dateToDeparture"],$_SESSION["prix"]),$passagers)
+    $data = array(array($_SESSION["nbPass"],$_SESSION["id_Vol"],$_SESSION["villeDep"],$_SESSION["villeArr"],$_SESSION["heureDep"],$vol["dateToDeparture"],$_SESSION["prix"]),$passagers);
     
     $json = json_encode($data);
     echo $json;
@@ -146,9 +53,15 @@ function infoConfirmation($bdd){
 }
 
 if($_GET["func"]=="infoConfirmation"){
-    infoConfirmation($bdd);
+    //infoConfirmation();
 }
 if($_GET["func"]=="infoVol"){
     infoVol();
 }
+if($_GET["func"]=="getPass"){
+    getPass();
+}
+
+
+
 ?>
