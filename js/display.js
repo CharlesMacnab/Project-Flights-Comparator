96be1a4c7a33766d061feb07db85d25b63a3f447
ThomasParragi <66598277+ThomasParragi@users.noreply.github.com>
2020-06-11 13:46:44 +0200

    Add files via upload

---
+++ js/display.js
@@ -0,0 +1,39 @@
+'use strict';
+
+ajaxRequest('GET', 'BDD/request.php/vols/', displayVols);
+ajaxRequest('GET', 'BDD/request.php/depart/', autocompleteDepart);
+
+function displayVols(vols) {
+    //console.log(vols);
+    /*
+    for(let vol of vols) {
+        console.log(vol.id);
+        console.log(vol.title);
+    }
+     */
+}
+
+function autocompleteDepart(villes) {
+
+    var liste = [];
+    //alert(villes);
+    //alert(liste);
+
+    for(let ville of villes){
+        //console.log(ville);
+        //alert(ville);
+        //console.log(ville.depVille);
+        //alert(ville.depVille);
+        liste.push(ville.city);
+        //console.log(liste);
+        //alert(liste);
+    }
+
+    alert(liste);
+
+    $('#depart').autocomplete({
+        source : liste,
+        minLength: 3,
+		delay: 200
+    });
+}
